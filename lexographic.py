625fa83577b201ccda7492795fa190a744120469
Hean Loong Ong <loongx2@gmail.com>
2019-09-11 00:36:05 +0800

    Update lexographic.py
    update with from itertools import combinations_with_replacement

--- lexographic.py
+++ lexographic.py
@@ -1,9 +1,16 @@
 from itertools import combinations
 
+from itertools import combinations_with_replacement
+
 if __name__ == '__main__':
     inputstr = str(input()).split(' ')
 
     tmp = sorted(inputstr[0])
     val = int(inputstr[1])
+	
+	for i in range(1, val+1):
+    	for txt in combinations(tmp, i):
+        	print ''.join(txt)
 
-    [print (''.join(i)) for x in range(1, val+1) for i in combinations(tmp, x)]
+    for txt in combinations_with_replacement(tmp, val):
+    print(''.join(txt))
